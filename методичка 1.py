0d8e6603d7a4598b499e3eb52b8593a9a551df51
Jo Morelly <morelly@bk.ru>
2021-06-23 21:26:35 +0300

    test

--- методичка 1.py
+++
@@ -1,9 +0,0 @@
-original_pass = "x1x2x3"
-password = input("Введите пароль: ")
-access = False
-
-if password == original_pass:
-    print("Пароль принят")
-    access = True
-else:
-    print("Пароль неверный")
