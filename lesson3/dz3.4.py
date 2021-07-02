9830fc84960c6e7559f6ecb9479d327758103074
Jo Morelly <morelly@bk.ru>
2021-07-02 16:10:58 +0300

    доделал 4 и 5 задание.

--- lesson3/dz3.4.py
+++ lesson3/dz3.4.py
@@ -10,7 +10,7 @@ def my_func(x, y):
 print(my_func(input("Введите число: "), input("Введите число: ")))
 
 
-моя попытка без просмотра видео, но всё равно использовал **, не получилось
+моя попытка без просмотра видео, использовал **, но не получилось
 """
 
 
@@ -18,7 +18,22 @@ def my_func(x, y):
     try:
         x = float(x)
         y = int(y)
-        if
+        if x <= 0 or y >= 0:
+
+            return "Выполните корректный ввод, где x > 0, y < 0"
+        else:
+            result = 1
+            for _ in range(abs(y)):
+                result /= x
+            return  f"Результат возведения х в степерь у: {round(result, 4)} "
+    except ValueError:
+        return "Вводите только числа"
+
+number_1 = input("Введите положительное число х: ")
+number_2 = input("Введите отрицательное число у: ")
+print(my_func(number_1, number_2))
+
+
 
 
 
